55ba788f7d0843dd4367d6f2a91b9a93250aa2e0
CodingBot000 <pokerface582@gmail.com>
2025-06-22 02:56:31 +0900

    Skip add-info form for already logged-in users

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -24,80 +24,70 @@ export async function GET(request: Request) {
       console.log(`auth callback route.ts userInfo: ${user}`);
       console.log(`auth callback route.ts userError: ${userError}`);
       if (!userError && user) {
+        const { id } = user;
+        
+        const { data: userRowExist, error: userRowExistError, count } = await supabase
+        .from(TABLE_MEMBERS)
+        .select("uuid", { count: "exact", head: true }) // head: true → data 없이 count만
+        .eq("uuid", id);
+      
+        const alreadyJoinedUser = (count ?? 0) > 0;
+
+        
+
         const { email, user_metadata, app_metadata } = user;
         const { full_name, avatar_url } = user_metadata ?? {};
         const provider = app_metadata?.provider ?? "email";
-        // userInfo?.identities.map((identity) => {
-        //   if (identity.provider === "google") {
-        //     // const { data: userInfo } = await supabase.auth.getUserInfo();
-        //     // console.log(userInfo);
-        //     console.log(`identity.id: ${identity.id}`);
-        //     console.log(`identity.identity_id: ${identity.identity_id}`);
-        //     console.log(`identity.provider: ${identity.provider}`);
-        //     console.log(`identity.user_id: ${identity.user_id}`);
-        //     console.log(`identity.created_at: ${identity.created_at}`);
-            
-        //     console.log(`identity.identity_data: ${identity.identity_data}`);
-        //     console.log("identity_data (JSON):", JSON.stringify(identity.identity_data, null, 2));
-        //     // "avatar_url": "https://lh3.googleusercontent.com/a/ACg8ocI5D4EJWDxc1sGmAqUdN9mOLrP25IOFp0pGj5kzZctYzbpj9ahc=s96-c",
-        //     // "email": "pokerface582@gmail.com",
-        //     // "email_verified": true,
-        //     // "full_name": "Bot coding",
-        //     // "iss": "https://accounts.google.com",
-        //     // "name": "Bot coding",
-        //     // "phone_verified": false,
-        //     // "picture": "https://lh3.googleusercontent.com/a/ACg8ocI5D4EJWDxc1sGmAqUdN9mOLrP25IOFp0pGj5kzZctYzbpj9ahc=s96-c",
-        //     // "provider_id": "114433813927840384505",
-        //     // "sub": "114433813927840384505"
-        //     console.log(`identity.last_sign_in_at: ${identity.last_sign_in_at}`);
-        //     console.log(`identity.updated_at: ${identity.updated_at}`);
-            
-        //   }
-        // });
+      
         console.log(userInfo);
-        // 사용자 정보 DB에 upsert 소셜로그인에서는 생일, 성별 국가 등 정보가 이 타이밍에 가져올수없음
-        await supabase.from(TABLE_MEMBERS).upsert({
-          uuid: user.id, // auth.users.id → foreign key
-          email: user.email ?? "",
-          name: user.user_metadata?.full_name ?? "",         // 이름 (Google/Apple 등에서 옴)
-          nickname: user.user_metadata?.name ?? "",          // 일반적으로 닉네임 (Apple, Facebook 등)
-          avatar: user.user_metadata?.avatar_url ?? "",      // 프로필 사진
-          provider: user.app_metadata?.provider ?? "",       // "google", "apple", "email" 등
-          last_login_at: new Date().toISOString(),           // 현재 로그인 시간
-          updated_at: new Date().toISOString(),              // 레코드 수정 시간
+        // 유저가 이미 가입했는데, SNS계정 정보를 변경했다면, 기존 MEMBERS테이블의 정보를 업데이트해야할까??
+        //일단 개인정보이니 원하지않을수있으니 안하는게 맞을수도..
 
-          // birth_date: birthDateInput, // 예: "1990-01-01"
-          // gender: genderInput,         // 예: "male" | "female" | "other"
-          // secondary_email: secondaryEmailInput ?? null,
-        });
-        
+        if (!alreadyJoinedUser) {
+          // 사용자 정보 DB에 upsert 소셜로그인에서는 생일, 성별 국가 등 정보가 이 타이밍에 가져올수없음
+          await supabase.from(TABLE_MEMBERS).upsert({
+            uuid: user.id, // auth.users.id → foreign key
+            email: user.email ?? "",
+            name: user.user_metadata?.full_name ?? "",         // 이름 (Google/Apple 등에서 옴)
+            nickname: user.user_metadata?.name ?? "",          // 일반적으로 닉네임 (Apple, Facebook 등)
+            avatar: user.user_metadata?.avatar_url ?? "",      // 프로필 사진
+            provider: user.app_metadata?.provider ?? "",       // "google", "apple", "email" 등
+            last_login_at: new Date().toISOString(),           // 현재 로그인 시간
+            updated_at: new Date().toISOString(),              // 레코드 수정 시간
+
+            // birth_date: birthDateInput, // 예: "1990-01-01"
+            // gender: genderInput,         // 예: "male" | "female" | "other"
+            // secondary_email: secondaryEmailInput ?? null,
+          });
+          
 // 아래는 별도로
 // user_no
 // id_country
 
-        const { id } = user;
+        // const { id } = user;
 
-       //  user 테이블에서 추가정보 입력 여부 확인 소셜로그인에서는 생일, 성별 국가 등 정보가 가입하는 순간에는 갖고올수없어서 이미 입력했는지 여부를 확인해야함
-       const { data: userRow } = await supabase
-       .from(TABLE_MEMBERS)
-       .select("id_country, birth_date, gender, secondary_email") // 생년월일, 성별 등도 추가 가능
-       .eq("uuid", id)
-       .maybeSingle();
+          //  user 테이블에서 추가정보 입력 여부 확인 소셜로그인에서는 생일, 성별 국가 등 정보가 가입하는 순간에는 갖고올수없어서 이미 입력했는지 여부를 확인해야함
+          const { data: userRow } = await supabase
+          .from(TABLE_MEMBERS)
+          .select("id_country, birth_date, gender, secondary_email") // 생년월일, 성별 등도 추가 가능
+          .eq("uuid", id)
+          .maybeSingle();
 
 
-        const needsProfileSetup = !userRow?.id_country || !userRow?.birth_date || !userRow?.gender || !userRow?.secondary_email;
-        
-        console.log(`auth callback route.ts userRow: ${userRow}`);
-        console.log(`auth callback route.ts needsProfileSetup: ${needsProfileSetup}`);
-        //추가 정보 필요 → /onboarding/complete-profile로 리디렉션
-        if (needsProfileSetup) {
-                                         
-          const redirectUrl = `${origin}/onboarding/complete-profile?code=${id}/returnUrl=/`;
-          console.log("auth callbacek needsProfileSetup move to :", redirectUrl);
-          return NextResponse.redirect(redirectUrl);
+          const needsProfileSetup = !userRow?.id_country || !userRow?.birth_date || !userRow?.gender || !userRow?.secondary_email;
+          
+          console.log(`auth callback route.ts userRow: ${userRow}`);
+          console.log(`auth callback route.ts needsProfileSetup: ${needsProfileSetup}`);
+          //추가 정보 필요 → /onboarding/complete-profile로 리디렉션
+          if (needsProfileSetup) {
+                                          
+            const redirectUrl = `${origin}/onboarding/complete-profile?code=${id}/returnUrl=/`;
+            console.log("auth callbacek needsProfileSetup move to :", redirectUrl);
+            return NextResponse.redirect(redirectUrl);
+          }
+          console.log("auth callback this is if (needsProfileSetup) next step needsProfileSetup :", `${needsProfileSetup}`);
+
         }
-        console.log("auth callback this is if (needsProfileSetup) next step needsProfileSetup :", `${needsProfileSetup}`);
-
       }
       console.log("auth callback out 1");
       //세션 교환에 성공하면, 환경에 따라 origin + next 또는 x-forwarded-host + next로 리디렉션
